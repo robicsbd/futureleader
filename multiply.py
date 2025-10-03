af67677bccbb027a24bd51bbf0c6bd0440012b72
robicsbd <robicsbd@gmail.com>
2025-10-03 22:25:24 +0600

    Create multiply.py

---
+++ multiply.py
@@ -0,0 +1,7 @@
+def multiply(a, b):
+    return a * b
+
+if __name__ == "__main__":
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    print("Product is:", multiply(num1, num2))
